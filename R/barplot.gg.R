ed10ddf9708126f580220633064d745440cb5a0e
coop711 <coop711@icloud.com>
2016-09-17 03:39:54 +0900

    figures added, sizes modified

--- R/barplot.gg.R
+++ R/barplot.gg.R
@@ -1,19 +1,19 @@
 barplot.gg <-
-function(x, position, ggtitle = "", xlab = ""){
+function(x, position, base_family = "", ggtitle = "", xlab = ""){
   switch(position,
-         stack = barplot.gg.stack(x, ggtitle = "", xlab = ""),
-         dodge = barplot.gg.dodge(x, ggtitle = "", xlab = ""),
-         fill = barplot.gg.fill(x, ggtitle = "", xlab = ""))
+         stack = barplot.gg.stack(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab),
+         dodge = barplot.gg.dodge(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab),
+         fill = barplot.gg.fill(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab))
 }
 barplot.gg.stack <-
-function(df, base_family = "NanumGothic", ggtitle = "", xlab = ""){
+function(df, base_family = "", ggtitle = "", xlab = ""){
 x <- df[, 2]
 y <- unlist(tapply(df$Freq, x, cumsum))
 b1 <- ggplot(df, aes(x = x, y = Freq, fill = vote)) +
   geom_bar(stat = "identity", position = "stack")
 b2 <- b1 +
-  theme_bw(base_family = "NanumGothic") +
-  theme.kr +
+  theme_bw(base_family = base_family) +
+#  theme.kr +
   scale_x_discrete(name = xlab) +
   scale_y_continuous(name = "집계", breaks = NULL) +
   scale_fill_manual(name = "찬반", values = rainbow(2)[2:1], guide = guide_legend(reverse = TRUE))
@@ -23,14 +23,14 @@ b3 <- b2 +
 return(b3)
 }
 barplot.gg.dodge <-
-function(df, base_family = "NanumGothic", ggtitle = "", xlab = ""){
+function(df, base_family = "", ggtitle = "", xlab = ""){
 x <- df[, 2]
 y <- unlist(tapply(df$Freq, x, cumsum))
 b1 <- ggplot(df, aes(x = x, y = Freq, fill = vote)) +
   geom_bar(stat = "identity", position = "dodge")
 b2 <- b1 +
-  theme_bw(base_family = "NanumGothic") +
-  theme.kr +
+  theme_bw(base_family = base_family) +
+#  theme.kr +
   scale_x_discrete(name = xlab) +
   scale_y_continuous(name = "집계", breaks = NULL) +
   scale_fill_manual(name = "찬반", values = rainbow(2)[2:1])
@@ -40,14 +40,14 @@ b3 <- b2 +
 return(b3)
 }
 barplot.gg.fill <-
-function(df, base_family = "NanumGothic", ggtitle = "", xlab = ""){
+function(df, base_family = "", ggtitle = "", xlab = ""){
 x <- df[, 2]
 y <- unlist(tapply(df$Freq, x, function(x){cumsum(x)/sum(x)}))
 b1 <- ggplot(df, aes(x = x, y = Freq, fill = vote)) +
   geom_bar(stat = "identity", position = "fill")
 b2 <- b1 +
-  theme_bw(base_family = "NanumGothic") +
-  theme.kr +
+  theme_bw(base_family = base_family) +
+#  theme.kr +
   scale_x_discrete(name = xlab) +
   scale_y_continuous(name = "집계", breaks = NULL) +
   scale_fill_manual(name = "찬반", values = rainbow(2)[2:1], guide = guide_legend(reverse = TRUE))
