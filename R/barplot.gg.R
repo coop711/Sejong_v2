159e2fea6d23ec72aed930ebbbfa2705ed6daac5
Kee-Won Lee <kwlee@hallym.ac.kr>
2017-04-20 02:10:30 +0900

    ylab, fill.name added

--- R/barplot.gg.R
+++ R/barplot.gg.R
@@ -1,12 +1,12 @@
 barplot.gg <-
-function(x, position, base_family = "", ggtitle = "", xlab = ""){
+function(x, position, base_family = "", ggtitle = "", xlab = "", ylab = "", fill.name = ""){
   switch(position,
-         stack = barplot.gg.stack(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab),
-         dodge = barplot.gg.dodge(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab),
-         fill = barplot.gg.fill(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab))
+         stack = barplot.gg.stack(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab, ylab = "", fill.name = ""),
+         dodge = barplot.gg.dodge(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab, ylab = "", fill.name = ""),
+         fill = barplot.gg.fill(x, base_family = base_family, ggtitle = ggtitle, xlab = xlab, ylab = "", fill.name = ""))
 }
 barplot.gg.stack <-
-function(df, base_family = "", ggtitle = "", xlab = ""){
+function(df, base_family = "", ggtitle = "", xlab = "", ylab = "", fill.name = ""){
 x <- df[, 2]
 y <- unlist(tapply(df$Freq, x, cumsum))
 b1 <- ggplot(df, aes(x = x, y = Freq, fill = rev(vote))) +
@@ -15,15 +15,15 @@ b2 <- b1 +
   theme_bw(base_family = base_family) +
 #  theme.kr +
   scale_x_discrete(name = xlab) +
-  scale_y_continuous(name = "집계", breaks = NULL) +
-  scale_fill_manual(name = "찬반", values = rainbow(2)[2:1], guide = guide_legend())
+  scale_y_continuous(name = ylab, breaks = NULL) +
+  scale_fill_manual(name = fill.name, values = rainbow(2)[2:1], guide = guide_legend())
 b3 <- b2 +
   geom_text(aes(y = y/2), label = format(df$Freq, big.mark = ","), position = position_stack()) +
   ggtitle(ggtitle)
 return(b3)
 }
 barplot.gg.dodge <-
-function(df, base_family = "", ggtitle = "", xlab = ""){
+function(df, base_family = "", ggtitle = "", xlab = "", ylab = "", fill.name = ""){
 x <- df[, 2]
 y <- unlist(tapply(df$Freq, x, cumsum))
 b1 <- ggplot(df, aes(x = x, y = Freq, fill = rev(vote))) +
@@ -32,15 +32,15 @@ b2 <- b1 +
   theme_bw(base_family = base_family) +
 #  theme.kr +
   scale_x_discrete(name = xlab) +
-  scale_y_continuous(name = "집계", breaks = NULL) +
-  scale_fill_manual(name = "찬반", values = rainbow(2)[2:1])
+  scale_y_continuous(name = ylab, breaks = NULL) +
+  scale_fill_manual(name = fill.name, values = rainbow(2)[2:1])
 b3 <- b2 +
   geom_text(aes(y = df$Freq/2), label = format(df$Freq, big.mark = ","), position = position_dodge(width = 0.9)) +
   ggtitle(ggtitle)
 return(b3)
 }
 barplot.gg.fill <-
-function(df, base_family = "", ggtitle = "", xlab = ""){
+function(df, base_family = "", ggtitle = "", xlab = "", ylab = "", fill.name = ""){
 x <- df[, 2]
 y <- unlist(tapply(df$Freq, x, function(x){cumsum(x)/sum(x)}))
 b1 <- ggplot(df, aes(x = x, y = Freq, fill = rev(vote))) +
@@ -49,8 +49,8 @@ b2 <- b1 +
   theme_bw(base_family = base_family) +
 #  theme.kr +
   scale_x_discrete(name = xlab) +
-  scale_y_continuous(name = "집계", breaks = NULL) +
-  scale_fill_manual(name = "찬반", values = rainbow(2)[2:1], guide = guide_legend())
+  scale_y_continuous(name = ylab, breaks = NULL) +
+  scale_fill_manual(name = fill.name, values = rainbow(2)[2:1], guide = guide_legend())
 b3 <- b2 +
   geom_text(aes(y = y/2), label = format(df$Freq, big.mark = ","), position = position_stack()) +
   ggtitle(ggtitle)
