4122c456de22100d285be77540e4b075b668c6cd
coop711 <coop711@icloud.com>
2017-04-21 08:57:33 +0900

    indentation for *.R

--- R/mosaic.gg.R
+++ R/mosaic.gg.R
@@ -1,4 +1,8 @@
-mosaic_gg <- function(tbl, base_family = "", ggtitle = "", xlab = "", ylab = "", fill.name = ""){
+mosaic_gg <- function(tbl, base_family = "", 
+                      ggtitle = "", 
+                      xlab = "", 
+                      ylab = "", 
+                      fill.name = ""){
 tbl.df <- as.data.frame(tbl)
 tbl.sum <- tapply(tbl.df$Freq, tbl.df[, 2], sum)
 tbl.p.m <- prop.table(tbl.sum)
@@ -16,28 +20,28 @@ m1 <- ggplot(tbl.p.df, aes(x = center, y = height, width = width)) +
            col = "white", 
            size = 1, 
            position = position_stack(reverse = TRUE)) 
-m1
 m2 <- m1 + 
   theme_bw(base_family = base_family)
-m2
 m3 <- m2 + 
   geom_text(aes(x = center, y = 1.05), 
             label = tbl.p.df[, 2], 
             family = base_family)
-m3
 m4 <- m3 + 
   geom_text(aes(x = center, y = label.height/2), 
             label = format(tbl.df$Freq, big.mark = ","), 
             position = position_stack(reverse = TRUE))
-m4
 x.breaks <- c(0, ifelse(cumsum(tbl.p.m) < 0.1, 0.0, cumsum(tbl.p.m)))
 x.label <- format(x.breaks, digits = 2, nsmall = 2)
 m5 <- m4 + 
-  scale_x_continuous(name = xlab, breaks = x.breaks, label = x.label) + 
+  scale_x_continuous(name = xlab, 
+                     breaks = x.breaks, 
+                     label = x.label) + 
   scale_y_continuous(name = ylab) + 
-  scale_fill_manual(name = fill.name, values = rainbow(2)[2:1], labels = tbl.df$vote, guide = guide_legend()) +
+  scale_fill_manual(name = fill.name, 
+                    values = rainbow(2)[2:1], 
+                    labels = tbl.df$vote, 
+                    guide = guide_legend()) +
   ggtitle(ggtitle) +
   theme(plot.margin = unit(c(1, 2, 1, 1), "lines"))
-m5
 return(m5)
 }
--- R/pie.gg.R
+++ R/pie.gg.R
@@ -5,9 +5,13 @@ function(df, ggtitle = "", font.family = ""){
   pie.label <- paste(levels(df$vote), format(df$Freq, big.mark = ","), sep = "\n") 
   p1 <- ggplot(df, aes(x = "", y = Freq, fill = vote)) 
   p2 <- p1 + 
-    geom_bar(width = 1, stat = "identity", position = position_stack(reverse = TRUE))
+    geom_bar(width = 1, 
+             stat = "identity", 
+             position = position_stack(reverse = TRUE))
   pie.1 <- p2 + 
-    coord_polar(theta = "y", start = 3*pi/2, direction = -1)
+    coord_polar(theta = "y", 
+                start = 3*pi/2, 
+                direction = -1)
   pie.2 <- pie.1 + 
     scale_y_continuous(name = "", breaks = NULL) +
     scale_x_discrete(name = "") 
@@ -18,7 +22,10 @@ function(df, ggtitle = "", font.family = ""){
   pie.5 <- pie.4 +
     guides(fill = "none")
   pie.6 <- pie.5 +
-    geom_text(aes(y = y.coord/n), label = pie.label, family = font.family, position = position_stack(reverse = TRUE))
+    geom_text(aes(y = y.coord/n), 
+              label = pie.label, 
+              family = font.family, 
+              position = position_stack(reverse = TRUE))
   pie.7 <- pie.6 +
     ggtitle(ggtitle) + 
     theme(plot.margin = unit(c(1, 1, 1.5, 1), "lines"))
